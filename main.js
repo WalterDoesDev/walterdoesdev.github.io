523a020bd64862dfa4f9b28508b5f69d3d4cd51d
WalterDoesDev <katie.warriner.2023@gmail.com>
2026-02-05 19:40:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const loadVideo = (iframe) => {
+    alert("JS LOADED");
+
+    const cid = "UC8eqKZX3qF8Nf-x6C55caVQ";
+
+    const channelURL = encodeURIComponent(
+        `https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`
+    );
+
+    const reqURL =
+    `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
+
+    const videoNumber = iframe.getAttribute("vnum");
+
+    fetch(reqURL)
+    .then(response => response.json())
+    .then(result => {
+        const link = result.items[videoNumber].link;
+        const id = link.split("=").pop();
+
+        iframe.src =
+        `https://www.youtube.com/embed/${id}?controls=0&autoplay=0`;
+    })
+    .catch(err => console.error(err));
+};
+
+// grab all iframes from YOUR html
+const iframes = document.getElementsByClassName("latestVideoEmbed");
+
+// call the function once per iframe
+for (let i = 0; i < iframes.length; i++) {
+    loadVideo(iframes[i]);
+}
